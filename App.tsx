de0134506a4a4f20486343cc86ac50f206bdddcb
Ale Guzman <84918983+luis-cicada@users.noreply.github.com>
2024-02-06 11:44:52 -0600

    Adding splash screen module (#4)

--- App.tsx
+++ App.tsx
@@ -3,8 +3,14 @@ import './global.css'
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
 import BaseScreen from './app/screens/base/BaseScreens'
+import SplashScreen from 'react-native-splash-screen'
+import { Platform } from 'react-native'
 
 const App: React.FC = () => {
+  React.useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <NavigationContainer>
       <BaseScreen />
--- ios/neuralnote/AppDelegate.mm
+++ ios/neuralnote/AppDelegate.mm
@@ -1,17 +1,21 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import "RNSplashScreen.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"neuralnote";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
+
   self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  bool didLaunchFinish = [super application:application didFinishLaunchingWithOptions:launchOptions];
+
+  [RNSplashScreen show];
+  
+  return didLaunchFinish;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
